eecf9faf9e7ab070e3d55b8ec97f8b293586858b
FabioSetubalDesing <71456089+FabioSetubalDesing@users.noreply.github.com>
2022-03-20 16:42:22 -0300

    SubClass

--- SubClasses.js
+++ SubClasses.js
@@ -1,18 +1,18 @@
 //criando uma Super Classe
 class Animal{
-    constructor(name){
+    constructor(name,sons){
         this.name=name
+        this.sons=sons
     }
     eat(){
         console.log(this.name + " está comendo");
+        console.log("SOM DE " + this.name +" "+ this.sons);
     }
 }
-
 //Sub Classe
 // extends serve para herda os recursos de uma class
 class Cat extends Animal{
-
 }
-//criando um objeto cat com as funcoes do Animal
-var cat = new Cat("Patricia");
+//criando um objeto cat e chamndo as funcoes do Animal
+var cat = new Cat("Patricia", "Meow Meow Meow");
 cat.eat();
